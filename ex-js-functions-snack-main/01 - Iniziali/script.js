31fb46ffd2fe64c5ffd757e77cec0619d259118c
MattiaPlacido <placido.mattia@gmail.com>
2024-10-21 15:41:03 +0200

    primo esercizio concluso con bonus

--- ex-js-functions-snack-main/01 - Iniziali/script.js
+++ ex-js-functions-snack-main/01 - Iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+const getInitials = (namesArray) => {
+  const initialsArray = [];
+  for (let i = 0; i < namesArray.length; i++)
+    initialsArray.push(namesArray[i].charAt(0));
+  return initialsArray;
+};
 
+//funzione che utilizza le mappe al posto del for
+// const getInitials = (namesArray) => namesArray.map((name) => name.charAt(0));
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const initials = getInitials(names);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(initials);
