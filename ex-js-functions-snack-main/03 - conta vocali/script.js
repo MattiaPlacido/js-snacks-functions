cded11d7a4faaabb4b0fd9f08bd60bf26eb13334
MattiaPlacido <placido.mattia@gmail.com>
2024-10-21 16:29:22 +0200

    Esercizio 3 concluso

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+const isVocal = (letter) =>
+  ["A", "B", "C", "D", "E"].includes(letter.toUpperCase());
 
+const extractVocals = (word) => {
+  const vocals = [];
+  for (let i = 0; i < word.length; i++)
+    if (isVocal(word[i])) vocals.push(word[i]);
+  return vocals;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const vocals = extractVocals(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+console.log(vocals);
