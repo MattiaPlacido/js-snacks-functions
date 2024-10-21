a635cfee0444562c37ae62b0e7bee331de753f14
MattiaPlacido <placido.mattia@gmail.com>
2024-10-21 16:43:05 +0200

    Quinto esercizio concluso

--- ex-js-functions-snack-main/05 - bonus/script.js
+++ ex-js-functions-snack-main/05 - bonus/script.js
@@ -5,14 +5,23 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+const customGreeting = (name) => {
+  const time = new Date();
+  const hour = time.getHours();
+  let output;
 
+  if (hour < 13) output = `Buongiorno ${name}`;
+  else if (hour < 17) output = `Buon pomeriggio ${name}`;
+  else output = `Buonasera ${name}`;
+
+  return output;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const greeting = customGreeting(name);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+console.log(greeting);
